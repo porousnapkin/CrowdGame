77f9b101baa8cc9b63ee834e9300d06470fc46f9
Will Swannack <porousnapkin@gmail.com>
2017-01-21 20:45:43 -0800

    created a data system for managing level length.

--- ASsets/Scripts/Editor/CrowdDataCreator.cs
+++ ASsets/Scripts/Editor/CrowdDataCreator.cs
@@ -11,4 +11,15 @@ public class CrowdDataCreator : Editor {
         EditorUtility.FocusProjectWindow();
         Selection.activeObject = moveData;
     }
+
+    [MenuItem("Custom/Create Enemy Spawning Data")]
+    public static void CreateEnemySpawningData()
+    {
+        var moveData = ScriptableObject.CreateInstance<EnemySpawningSet>();
+        AssetDatabase.CreateAsset(moveData, "Assets/Data/EnemySpawningSet.asset");
+        EditorUtility.SetDirty(moveData);
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = moveData;
+    }
 }
--- ASsets/Scripts/LaserSpawner.cs
+++ ASsets/Scripts/LaserSpawner.cs
@@ -1,52 +1,19 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class LaserSpawner : MonoBehaviour {
+public class LaserSpawner : EnemySpawner {
     public GameObject laserPrefab;
-    public GameObject grenadePrefab;
     public int xRange = 20;
     public int yRange = 10;
+    public float chanceForHorizontalLaser = 0.4f;
 
-	void Start () {
-        StartCoroutine(LaserCoroutine());
-	}
-	
-    IEnumerator LaserCoroutine()
-    {
-        while(true)
-        {
-            yield return new WaitForSeconds(Random.Range(2.5f, 3.0f));
-            //CreateGrenade();
-            CreateRandomLaser();
-        }
-    }
-
-    void CreateRandomLaser()
+    public override void Spawn()
     {
         var val = Random.value;
-        if(val < 0.4f)
-        {
+        if(val < chanceForHorizontalLaser)
             CreateHorizontalLaser();
-        }
-        else if (val < 0.8f)
-        {
+        else 
             CreateVerticalLaser();
-        }
-        else if(val < 0.95f)
-        {
-            CreateHorizontalLaser();
-            CreateVerticalLaser();
-        }
-        else if(val < 0.97f)
-        {
-            CreateHorizontalLaser();
-            CreateHorizontalLaser();
-        }
-        else
-        {
-            CreateVerticalLaser();
-            CreateVerticalLaser();
-        }
     }
 
     void CreateHorizontalLaser()
@@ -63,12 +30,5 @@ public class LaserSpawner : MonoBehaviour {
         var laser = laserGO.GetComponent<Laser>();
         laser.orientation = Laser.Orientation.Vertical;
         laser.rootPosition = new Vector2(Random.Range(-xRange, xRange), 0);
-    }
-
-    void CreateGrenade()
-    {
-        var grenadeGO = GameObject.Instantiate(grenadePrefab, transform);
-        var grenade = grenadeGO.GetComponent<Grenade>();
-        grenade.rootPosition = new Vector2(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange));
     }
 }
